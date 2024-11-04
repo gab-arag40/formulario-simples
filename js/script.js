aba83332f39d9e1736c3e313f9b0dd5776eb1486
Gabriel Aragão <gabriel.arag40@gmail.com>
2024-11-04 18:14:43 -0300

    adicionando verifição

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 function enviar(){
-    alert ("Formulário Enviado")
+    if(document.getElementById("cxnome").value==""){
+        alert("Preencha o campo 'nome' corretamente.");
+    }
+    else if(document.getElementById("cxemail").value==""){
+        alert("Preencha o campo 'email' corretamente.");
+    }
+    else if(document.getElementById("cxsenha").value==""){
+        alert("Preencha o campo 'senha' corretamente.");
+    }
+    else if(document.getElementById("cxsenha2").value=="" || document.getElementById("cxsenha2").value != document.getElementById("cxsenha").value){
+        alert("Preencha o campo 'confirmação de senha' corretamente.");
+    }
+    else if(document.getElementById("cxestcid").value=="padrao2"){
+        alert("Selecione o campo 'cidade es' corretamente.");
+    }
+    else if(document.getElementById("cxgenero").value=="padrao"){
+        alert("selecione o campo 'genero' corretamente.");
+    }
+    else if(document.getElementById("cxdata").value==""){
+        alert("Preencha o campo 'data de nascimento' corretamente.");
+    }
+    else if(document.getElementById("cxdesc").value==""){
+        alert("Preencha o campo 'descrição pessoal' corretamente.");
+    }
+    else{
+        alert("Enviado com sucesso!");
+        location.reload();
+    }
 }
