6aeb5a502d696dde5aeb47864615d03728209d88
Gabriel Aragão <gabriel.arag40@gmail.com>
2024-11-04 18:23:43 -0300

    adicionando verifição 2

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 function enviar(){
     if(document.getElementById("cxnome").value==""){
-        alert("Preencha o campo 'nome' corretamente.");
+        alert("Preencha o campo 'Nome Completo' corretamente.");
     }
     else if(document.getElementById("cxemail").value==""){
-        alert("Preencha o campo 'email' corretamente.");
+        alert("Preencha o campo 'E-mail' corretamente.");
     }
     else if(document.getElementById("cxsenha").value==""){
-        alert("Preencha o campo 'senha' corretamente.");
+        alert("Preencha o campo 'Senha' corretamente.");
     }
     else if(document.getElementById("cxsenha2").value=="" || document.getElementById("cxsenha2").value != document.getElementById("cxsenha").value){
-        alert("Preencha o campo 'confirmação de senha' corretamente.");
+        alert("Campo 'Confirmação Senha' não foi preenchido corretamente.");
     }
     else if(document.getElementById("cxestcid").value=="padrao2"){
-        alert("Selecione o campo 'cidade es' corretamente.");
+        alert("Selecione o campo 'Cidade - Estado' corretamente.");
     }
     else if(document.getElementById("cxgenero").value=="padrao"){
-        alert("selecione o campo 'genero' corretamente.");
+        alert("Selecione o campo 'Gênero' corretamente.");
     }
     else if(document.getElementById("cxdata").value==""){
-        alert("Preencha o campo 'data de nascimento' corretamente.");
+        alert("Preencha o campo 'Data de Nascimento' corretamente.");
     }
     else if(document.getElementById("cxdesc").value==""){
-        alert("Preencha o campo 'descrição pessoal' corretamente.");
+        alert("Preencha o campo 'Descrição Pessoal' corretamente.");
     }
     else{
         alert("Enviado com sucesso!");
